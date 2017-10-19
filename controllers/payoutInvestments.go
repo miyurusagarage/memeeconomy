3f23e9dd0725c54d5ab8c6bf71252cf18e6c4051
miyurusagarage <miyuru.s@intellogic.lk>
2017-10-19 17:33:21 +0530

    payout investment fixes

--- controllers/payoutInvestments.go
+++ controllers/payoutInvestments.go
@@ -4,6 +4,7 @@ import (
 	"github.com/miyurusagarage/memeeconomy/models"
 	"time"
 	"math"
+	"github.com/miyurusagarage/memeeconomy/shared"
 )
 
 type PayoutInvestmentsController struct {
@@ -11,33 +12,42 @@ type PayoutInvestmentsController struct {
 }
 
 func (c *PayoutInvestmentsController) Prepare() {
-	c.Authorize()
+
 }
 
 func (c *PayoutInvestmentsController) Get() {
 	eligibleMemes, _ := models.GetToBeExpiredMemes()
-	for _, eligibleMeme := range *eligibleMemes {
-		eligibleMeme.IsExpired = true
-		eilgibleInvestments, _ := models.GetMemeInvestmentsByMeme(eligibleMeme.Key.Name)
-		for _, eligibleMemeInvestment := range *eilgibleInvestments {
-			eligibleMemeInvestment.PayoutDate = time.Now()
-			eligibleMemeInvestment.PayoutAmount = getPayoutAmount(eligibleMeme, eligibleMemeInvestment)
-			investmentUser, _ := models.GetUserFromId(eligibleMemeInvestment.UserId)
-			investmentUser.CurrentCredit += eligibleMemeInvestment.PayoutAmount
-			eligibleMemeInvestment.Update()
-			investmentUser.Update()
+	if eligibleMemes != nil {
+		for _, eligibleMeme := range *eligibleMemes {
+			eligibleMeme.IsExpired = true
+			eligibleInvestments, _ := models.GetMemeInvestmentsByMeme(eligibleMeme.Key.Name)
+			if eligibleInvestments != nil {
+
+				for _, eligibleMemeInvestment := range *eligibleInvestments {
+					eligibleMemeInvestment.PayoutDate = time.Now()
+					eligibleMemeInvestment.PayoutAmount = getPayoutAmount(eligibleMeme, eligibleMemeInvestment)
+					investmentUser, _ := models.GetUserFromId(eligibleMemeInvestment.UserId)
+					investmentUser.CurrentCredit += eligibleMemeInvestment.PayoutAmount
+					eligibleMemeInvestment.Update()
+					investmentUser.Update()
+				}
+			}
+			eligibleMeme.Update()
 		}
-		eligibleMeme.Update()
 	}
+	jsonResponse := new(shared.JSONResponse)
+	jsonResponse.Message = "Success"
+	c.Data["json"] = jsonResponse
+	c.ServeJSON()
 }
 
-func getPayoutAmount(meme models.Meme, memeInvestment models.MemeInvestment) int{
-	moment :=float64(memeInvestment.MomentsTotalFame - memeInvestment.BidAmount)
+func getPayoutAmount(meme models.Meme, memeInvestment models.MemeInvestment) int {
+	moment := float64(memeInvestment.MomentsTotalFame - memeInvestment.BidAmount)
 	bid := float64(memeInvestment.BidAmount)
-	total :=float64(meme.TotalFame)
+	total := float64(meme.TotalFame)
 	gain := total - moment - bid
 	absTotal := math.Abs(float64(total))
 	fame := (bid / absTotal * gain) + bid
-	fame = math.Max(fame,0)
+	fame = math.Max(fame, 0)
 	return int(fame)
 }
